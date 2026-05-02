49941e85d69f3c31385c477de31a6266780b973c
Hami Ibriyamov <hamop3108@gmail.com>
2026-05-02 15:22:18 +0200

    Refactor eigenvalue calculation and fix variable names

--- problems/calculate_eigenvalues_of_a_matrix.py
+++ problems/calculate_eigenvalues_of_a_matrix.py
@@ -1,17 +1,23 @@
 def calculate_eigenvalues(matrix: list[list[float|int]]) -> list[float]:
-	a = matrix[0][0]
-	b = matrix[0][1]
-	c = matrix[1][0]
-	d = matrix[1][1]
+    a = matrix[0][0]
+    b = matrix[0][1]
+    c = matrix[1][0]
+    d = matrix[1][1]
 
-	tr = a + d
-	det = (a*d) - (b*c)
+    # Find the trace of the 2x2 matrix
+    tr = a + d
 
-	eigenvectors = []
-	for i in range(9):
-		for j in range(9):
-			if i * j == det and i + j == tr:
-				eigenvectors.append(i)
-	
-	eigenvectors = sorted(eigenvectors, reverse=True)
-	return eigenvectors
+    # Find the determinant of the 2x2 matrix
+    det = (a*d) - (b*c)
+
+    # Find the eigenvalues
+    eigenvalues = []
+    for i in range(9):
+        for j in range(9):
+            if i * j == det and i + j == tr:
+                eigenvalues.append(i)    
+    eigenvalues = sorted(eigenvalues, reverse=True)
+    return eigenvalues
+    
+
+print(calculate_eigenvalues([[2, 1], [1, 2]]))
