70b787119f0e7b8539d22d3afa7b48ff549db8b9
Hami Ibriyamov <hamop3108@gmail.com>
2026-07-02 15:34:46 +0200

    Fix Q-learning update assignment in interaction_step

--- Q-Learning on FrozenLake from Scratch/Training Loop/interaction_step.py
+++ Q-Learning on FrozenLake from Scratch/Training Loop/interaction_step.py
@@ -2,5 +2,5 @@ def interaction_step(env, q_table, state, epsilon, alpha, gamma, rng):
     # TODO: select epsilon-greedy action, step env, apply Q-learning update, return (next_state, reward, done)
     new_action = epsilon_greedy_action(q_table, state, epsilon, env.action_space, rng)
     obs, reward, terminated, truncated , info = env.step(new_action)
-    q_table = q_learning_update(q_table, state, new_action, reward, obs, terminated, alpha, gamma)
+    q_learning_update(q_table, state, new_action, reward, obs, terminated, alpha, gamma)
     return (int(obs), float(reward), bool(terminated))
