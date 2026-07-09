800be2a8d2d40380e73fbac38d29b3c473673048
Hami Ibriyamov <hamop3108@gmail.com>
2026-07-09 13:47:34 +0200

    Add function to create an empty Tic-Tac-Toe board

---
+++ Reinforcement Learning for Tic-Tac-Toe: From Minimax to DQN/Board Representation & Game Engine/create_empty_board.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def create_empty_board():
+    """Return an empty 3x3 Tic-Tac-Toe board as an int numpy array of zeros."""
+    # TODO: return a (3, 3) integer numpy array filled with zeros
+    return np.zeros(shape=(3,3), dtype=int)
