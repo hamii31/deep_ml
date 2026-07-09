50e468911ba89a6f9d0b690fd2b5a36147f10b16
Hami Ibriyamov <hamop3108@gmail.com>
2026-07-09 13:49:04 +0200

    Create is_cell_empty.py

---
+++ Reinforcement Learning for Tic-Tac-Toe: From Minimax to DQN/Board Representation & Game Engine/is_cell_empty.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def is_cell_empty(board, row, col):
+    """Return True if board[row, col] is empty (0), else False."""
+    # TODO: check whether the cell at (row, col) is empty
+    return board[row][col] == 0
