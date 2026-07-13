6419d5747217f2a99b9c10dcd44034eac97d3402
Hami Ibriyamov <hamop3108@gmail.com>
2026-07-13 09:51:19 +0200

    Delete Reinforcement Learning for Tic-Tac-Toe: From Minimax to DQN/Board Representation & Game Engine directory

--- Reinforcement Learning for Tic-Tac-Toe: From Minimax to DQN/Board Representation & Game Engine/encode_player.py
+++
@@ -1,6 +0,0 @@
-def encode_player(player):
-    """Return the integer encoding for 'X', 'O', or 'empty'."""
-    # TODO: map 'X' to 1, 'O' to -1, 'empty' to 0
-    if player == 'X': return 1
-    elif player == 'O': return -1
-    elif player == 'empty': return 0
